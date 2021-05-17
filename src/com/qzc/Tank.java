255c8470166413947af85c9f3544eb5689f51e88
qzc2015 <877731624@qq.com>
2021-05-17 15:33:19 +0800

    边间检测

--- src/com/qzc/Tank.java
+++ src/com/qzc/Tank.java
@@ -114,6 +114,15 @@ public class Tank {
 		if (this.group == Group.BAD && random.nextInt(100)>95) this.fire();
 		if (this.group == Group.BAD && random.nextInt(100)>95)
 			randomDir();
+
+		boundsCheck();
+	}
+
+	private void boundsCheck() {
+		if (x<2) x=2;
+		if (y<28) y=28;
+		if (x>TankFrame.GAME_WIDTH-Tank.WIDTH-2) x=TankFrame.GAME_WIDTH-Tank.WIDTH-2;
+		if (y>TankFrame.GAME_HEIGHT-Tank.HEIGHT-2) y=TankFrame.GAME_HEIGHT-Tank.HEIGHT-2;
 	}
 
 	private void randomDir() {
